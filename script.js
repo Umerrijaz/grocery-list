cae76e515d9971226baef624cf9d96aa7182cbc2
Umer Ijaz <umerijaaz@outlook.com>
2023-03-12 23:22:30 +0100

    comments added for the code

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
+// Clears the grocery list when the eraser icon is clicked
 document.querySelector("#eraser").addEventListener("click", () => {
-    document.querySelector("#groceryItems").textContent = "";
-  })
-  
-  document.querySelector("#userInput").addEventListener("keydown", (event) => {
-    if(event.key == "Enter")
-      addItem();
+  document.querySelector("#groceryItems").textContent = "";
+});
+
+// Adds an item to the grocery list when the "Enter" key is pressed in the input field
+document.querySelector("#userInput").addEventListener("keydown", (event) => {
+  if(event.key == "Enter")
+    addItem();
+});
+
+// Function to add a new item to the grocery list
+addItem = () => {
+  // Create a new <h2> element for the item
+  const item = document.createElement("h2");
+
+  // Set the text content of the <h2> element to the input value with a bullet point
+  item.textContent = "- " + document.querySelector("#userInput").value;
+
+  // Add a click event listener to the <h2> element to toggle line-through decoration
+  item.addEventListener("click", () => {
+    if(item.style.textDecoration != "line-through")
+      item.style.textDecoration = "line-through";
+    else
+      item.style.textDecoration = "none";
   });
-  
-  addItem = () => {
-    const item = document.createElement("h2")
-    item.textContent = "- " + document.querySelector("#userInput").value;
-  
-    item.addEventListener("click", () => {
-      if(item.style.textDecoration != "line-through")
-        item.style.textDecoration = "line-through";
-      else
-        item.style.textDecoration = "none";
-    })
-  
-    document.querySelector("#groceryItems").appendChild(item);
-    document.querySelector("#userInput").value = "";
-  }
+
+  // Append the <h2> element to the grocery list
+  document.querySelector("#groceryItems").appendChild(item);
+
+  // Reset the input field value to an empty string
+  document.querySelector("#userInput").value = "";
+};
